d491b99da1e3c842bb17f42fb34bb9808ed22016
DESKTOP-Q2BPRDF\NEXT <mariamyasser3092@gmail.com>
2023-12-18 00:52:17 +0200

    + finished PostActivity

--- app/src/main/java/basics/LoginActivity.java
+++ app/src/main/java/basics/LoginActivity.java
@@ -1,6 +1,9 @@
 package basics;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +15,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+      Button button= (Button) findViewById(R.id.btn1);
+  button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+         openPostActivity();
+      }
+  });
+    }
+    public void openPostActivity(){
+        Intent intent =new Intent(this ,PostActivity.class);
+        startActivity(intent);
     }
 }
